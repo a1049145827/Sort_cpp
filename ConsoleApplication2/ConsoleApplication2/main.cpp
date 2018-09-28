84ce1bea166bce61bb5f748008ec957c83b08f36
Hongbo Liu <a1049145827@hotmail.com>
2018-09-28 09:47:48 +0800

    条件编译，适配xcode

--- ConsoleApplication2/ConsoleApplication2/main.cpp
+++ ConsoleApplication2/ConsoleApplication2/main.cpp
@@ -6,7 +6,10 @@
 //  Copyright © 2018 qianbao. All rights reserved.
 //
 
+#if defined(_MSC_VER)
 #include "pch.h"
+#endif
+
 #include <iostream>
 #include "Student.h"
 #include "SortTestHelper.h"
@@ -248,3 +251,33 @@ int main()
 
     return 0;
 }
+
+// 条件编译：识别编译器
+//How to detect the compiler name
+//Compiler name macros indicate a specific compiler, such as Intel ICC or Microsoft Visual Studio. There are exceptions. See the notes after the table.
+//
+//#if defined(__clang__)
+///* Clang/LLVM. ---------------------------------------------- */
+//
+//#elif defined(__ICC) || defined(__INTEL_COMPILER)
+///* Intel ICC/ICPC. ------------------------------------------ */
+//
+//#elif defined(__GNUC__) || defined(__GNUG__)
+///* GNU GCC/G++. --------------------------------------------- */
+//
+//#elif defined(__HP_cc) || defined(__HP_aCC)
+///* Hewlett-Packard C/aC++. ---------------------------------- */
+//
+//#elif defined(__IBMC__) || defined(__IBMCPP__)
+///* IBM XL C/C++. -------------------------------------------- */
+//
+//#elif defined(_MSC_VER)
+///* Microsoft Visual Studio. --------------------------------- */
+//
+//#elif defined(__PGI)
+///* Portland Group PGCC/PGCPP. ------------------------------- */
+//
+//#elif defined(__SUNPRO_C) || defined(__SUNPRO_CC)
+///* Oracle Solaris Studio. ----------------------------------- */
+//
+//#endif
